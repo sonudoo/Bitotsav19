25f557954757a43b6948f24c8beb7e6643ea4d32
ygyash <ygyashgoyal@gmail.com>
2019-01-09 23:12:42 +0530

    Add registration front end

---
+++ js/registration.js
@@ -0,0 +1,15 @@
+$(window).ready(function(){
+    $("#reg-one").click(function(){
+        var data = {
+                    }
+
+    //trim karke bhejna hai
+    });
+    $("#reg-two").click(function(){
+
+    });
+    $("#reg-three").click(function(){
+
+    });
+
+})
