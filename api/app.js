28abaeeda311a0dcfa4badc4fe59981dab1432cc
Sushant <mya.sushant@yahoo.com>
2019-02-14 03:38:04 +0530

    Add event redirection to app

--- api/app.js
+++ api/app.js
@@ -69,6 +69,76 @@ app.get('/api/likes', function (req, res) {
     });
 })
 
+app.get('/event/:id', function (req, res){
+    db.events.find({eventId: parseInt(req.params.id)}, function(error, result){
+        if(error){
+            res.send(JSON.stringify({
+                success: false,
+                msg: "Database fetch error occured"
+            }))
+        }
+        else{
+            if(result.length != 1){
+                res.send(JSON.stringify({
+                    success: false,
+                    msg: "No such event found"
+                }));
+            }
+            else{
+                let event = result[0];
+                let data = `
+                <!doctype html>
+                    <html lang="en">
+                    <head>
+                        <!-- Required meta tags -->
+                        <meta charset="utf-8">
+                        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+                        <!-- Bootstrap CSS -->
+                        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+                        <title>${event.eventName}</title>
+                    </head>
+                    <body>
+                    <div class="container-fluid">
+                        <h3 style="text-align: center;">${event.eventName}</h3>
+                        <hr>
+                        <h6>Day</h6>
+                        <p>${event.eventDay}</p>
+                        <h6>Time</h6>
+                        <p>${event.eventTime}</p>
+                        <h6>Venue</h6>
+                        <p>${event.eventVenue}</p>
+                        <h6>Description</h6>
+                        <p>${event.eventDescription}</p>
+                        
+                        <h6>Rules</h6>
+                        <ul>`
+                let rules = event.eventRules.split("\n");
+                for(let i in rules){
+                    data += "<li>"+rules[i]+"</li>";
+                }
+                data += `
+                </div>
+                <br>
+                <div class="text-center">
+                    <a class="btn btn-primary" href="https://bitotsav.in/">Back to Home</a>
+                </div>
+                <br>
+                        <!-- Optional JavaScript -->
+                        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+                        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+                        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+                        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+                    </body>
+                    </html>
+                `;
+                res.send(data);
+            }
+        }
+    })
+})
+
 app.get('/api/backup/:password', function (req, res) {
 
     if (req.params.password != "1020415") {
